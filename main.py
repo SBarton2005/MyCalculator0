0b1f824aec1c5b32a2dd491c6f6672894b161f7d
SBarton2005 <sbarton2024@k12.andoverma.us>
2020-05-06 19:57:19 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+def add(a):
+  c = 0
+  for b in a:
+    c += b
+  return c
+def sub(a, b):
+  return a - b
+def mul(a):
+  c = 1
+  for b in a:
+    c = c * b
+  return c
+def div(a, b):
+  return a / b
+def rem(a, b):
+  return a % b
+def exp(a, b):
+  return a ** b
+def roo(a, b):
+  return a ** (1 / b)

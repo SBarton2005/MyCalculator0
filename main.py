40cfa1076a5e4a5f43e8ac5c7769559f706a082e
SBarton2005 <sbarton2024@k12.andoverma.us>
2020-05-06 20:01:09 +0000

    I added comments

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
 def add(a):
+  #This is addition
   c = 0
   for b in a:
     c += b
   return c
 def sub(a, b):
+  #This is subtraction
   return a - b
 def mul(a):
+  #This is multiplication
   c = 1
   for b in a:
     c = c * b
   return c
 def div(a, b):
+  #This is division
   return a / b
 def rem(a, b):
+  #This is finding the remainder
   return a % b
 def exp(a, b):
+  #This is exponentials
   return a ** b
 def roo(a, b):
+  #This is rooting
   return a ** (1 / b)

3ce02e6690a23dca4da4f8a94b150b144c4e1ced
SBarton2005 <sbarton2024@k12.andoverma.us>
2020-05-06 20:42:44 +0000

    Added a factorial opperator

--- main.py
+++ main.py
@@ -25,3 +25,29 @@ def exp(a, b):
 def roo(a, b):
   #This is rooting
   return a ** (1 / b)
+def fac(a):
+  #This is factorials
+  i = 1
+  while a > 0:
+    i = i * a
+    a -= 1
+  return i
+def cal(o, a, b = 0):
+  if o == "add":
+    return add(a)
+  elif o == "sub":
+    return sub(a, b)
+  elif o == "mul":
+    return mul(a)
+  elif o == "div":
+    return div(a, b)
+  elif o == "rem":
+    return rem(a, b)
+  elif o == "exp":
+    return exp(a, b)
+  elif o == "roo":
+    return roo(a, b)
+  elif o == "fac":
+    return fac(a)
+  else:
+    return "You didn't give an opperation we have"
